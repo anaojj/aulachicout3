4d499ea5e12701e453fba2f960e7670e09da17d4
joana <joohors0@gmail.com>
2024-04-08 21:56:28 -0300

    first commit

---
+++ src/main/java/edu/br/Atv3.java
@@ -0,0 +1,31 @@
+package edu.br;
+
+import java.io.*;
+
+public class Atv3 {
+
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("meuarquivo.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String texto;
+            while ((texto = bufferedReader.readLine()) != null) {
+                System.out.println(texto);
+            }
+
+            bufferedReader.close();
+
+            FileWriter fileWriter = new FileWriter("meuarquivo.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.newLine();
+            bufferedWriter.write("Adicionando texto ao arquivo 'meuarquivo.txt'");
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+}
+
